ddfb1fa1d133863086c464fcc3755ac7e02700a2
neyogiry <yrigoyen.fernando@gmail.com>
2021-06-09 13:08:39 -0500

    chore: Updated project architecture

--- Calculator/SceneDelegate.swift
+++ Calculator/SceneDelegate.swift
@@ -16,7 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Setup
+//        setupView()
+        setupView(windowScene: windowScene)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -45,6 +49,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+    }
+    
+    // MARK: Private methods
+    
+//    private func setupView() {
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        let vc = HomeViewController()
+//        window?.rootViewController = vc
+//        window?.makeKeyAndVisible()
+//    }
+    
+    private func setupView(windowScene: UIWindowScene) {
+        window = UIWindow(windowScene: windowScene)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
     }
 
 
